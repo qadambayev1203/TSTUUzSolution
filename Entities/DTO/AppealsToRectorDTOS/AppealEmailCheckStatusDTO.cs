2d1c7c7dc72ae2445c12788799a0315d20bdb774
qadambayev1203 <qadamboyevmominjon003@gmail.com>
2024-06-07 12:01:06 +0500

    upd

--- Entities/DTO/AppealsToRectorDTOS/AppealEmailCheckStatusDTO.cs
+++ Entities/DTO/AppealsToRectorDTOS/AppealEmailCheckStatusDTO.cs
@@ -13,6 +13,6 @@ namespace Entities.DTO.AppealsToRectorDTOS
         public int id { get; set; }
         public string appeal { get; set; }
         public StatusConfReadedDTO status_ { get; set; }
-
+        //aaaaaaa
     }
 }
